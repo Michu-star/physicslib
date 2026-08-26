04f95174f0f2c6d99645a3061d11d01b92ce566d
michal <michalzmichalowic@gmail.com>
2026-08-26 13:35:50 +0200

    Allow user to pass extra arguments

--- src/physicslib/ode.py
+++ src/physicslib/ode.py
@@ -32,19 +32,19 @@ def rk4_first_order(x, y0, derivative_y):
 
 
 # Helper function for Euler and RK4
-def derivative_state(x, state, last_derivative):
+def derivative_state(x, state, last_derivative, args=()):
     derivative = np.zeros_like(state)
 
     # y', ..., y^(n-1)
     derivative[:-1] = state[1:]
     # y^n
-    derivative[-1] = last_derivative(x, state)
+    derivative[-1] = last_derivative(x, state, *args)
 
     return derivative
 
 
 # These functions solve ODEs of any order
-def euler(x, y0, nth_derivative_y):
+def euler(x, y0, nth_derivative_y, args=()):
     dx = np.diff(x)
     n = len(y0)
 
@@ -53,12 +53,12 @@ def euler(x, y0, nth_derivative_y):
 
     for i in range(len(x) - 1):
         # y, y', ..., y^(n-1)
-        y[i + 1] = y[i] + derivative_state(x[i], y[i], nth_derivative_y) * dx[i]
+        y[i + 1] = y[i] + derivative_state(x[i], y[i], nth_derivative_y, args) * dx[i]
 
     return y
 
 
-def rk4(x, y0, nth_derivative_y):
+def rk4(x, y0, nth_derivative_y, args=()):
     dx = np.diff(x)
     n = len(y0)
 
@@ -67,19 +67,19 @@ def rk4(x, y0, nth_derivative_y):
 
     for i in range(len(x) - 1):
         k1 = derivative_state(x[i], y[i],
-                              nth_derivative_y)
+                              nth_derivative_y, args)
 
         k2 = derivative_state(x[i] + dx[i] / 2,
                               y[i] + k1 * dx[i] / 2,
-                              nth_derivative_y)
+                              nth_derivative_y, args)
 
         k3 = derivative_state(x[i] + dx[i] / 2,
                               y[i] + k2 * dx[i] / 2,
-                              nth_derivative_y)
+                              nth_derivative_y, args)
 
         k4 = derivative_state(x[i] + dx[i],
                               y[i] + k3 * dx[i],
-                              nth_derivative_y)
+                              nth_derivative_y, args)
 
         slope = (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
@@ -88,7 +88,7 @@ def rk4(x, y0, nth_derivative_y):
     return y
 
 
-def rk4_system(x, y0, state_derivative):
+def rk4_system(x, y0, state_derivative, args=()):
     dx = np.diff(x)
     n = len(y0)
 
@@ -96,16 +96,19 @@ def rk4_system(x, y0, state_derivative):
     y[0] = y0
 
     for i in range(len(x) - 1):
-        k1 = state_derivative(x[i], y[i])
+        k1 = state_derivative(x[i], y[i], *args)
 
         k2 = state_derivative(x[i] + dx[i] / 2,
-                              y[i] + k1 * dx[i] / 2)
+                              y[i] + k1 * dx[i] / 2,
+                              *args)
 
         k3 = state_derivative(x[i] + dx[i] / 2,
-                              y[i] + k2 * dx[i] / 2)
+                              y[i] + k2 * dx[i] / 2,
+                              *args)
 
         k4 = state_derivative(x[i] + dx[i],
-                              y[i] + k3 * dx[i])
+                              y[i] + k3 * dx[i],
+                              *args)
 
         slope = (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
